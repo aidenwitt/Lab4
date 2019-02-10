8fe8bf7b0cb560e153df359f0d7f1b5500ddd676
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-02-10 15:20:38 -0600

    Update Todo.java
    Final Todo

--- Todo.java
+++ Todo.java
@@ -1,18 +1,31 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-02-10
+OU ID_______________: 113472850
+OU EMAIL____________: aidenwitt@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Finish Animal equals method
+2. Test all of the methods in Animal Class (complete AnimalTest)
+3. Finish Equipment methods
+4. Write Equipment Test and test all methods
+5. Finish Zoo class
+6. Write ZooTest and test all methods
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 2
+2. 5
+3. 5
+4. 10
+5. 5
+6. 10
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 1
+2. 10
+3. 2
+4. 20
+5. 5
+6. 25
+
 */}
