17d2d8bcffc7d798e8f0986b3baea81a88db859a
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-02-10 15:18:47 -0600

    Update AnimalTest.java
    Final version of AnimalTest

--- AnimalTest.java
+++ AnimalTest.java
@@ -22,6 +22,25 @@ public class AnimalTest
         Assert.assertEquals(0, animal.getWeight(), 0.01);
     }
 
-    // TODO: test full constructor, getters, and toString
+    public void constructorTest() throws AssertException {
+    	Animal paul = new Animal("Red", "Paul", 150.0, 60.0);
+    	Animal paul2 = new Animal("Red", "Paul", 150.0, 60.0);
+    	Animal paul3 = new Animal("Blue", "Paul2", 324.0, 100.0);
+    	Object objectivePaul = new Object();
+    	Animal boringPaul = new Animal();
+    	
+    	Assert.assertEquals("Red", paul.getColor());
+    	Assert.assertEquals("Paul", paul.getName());
+    	Assert.assertEquals(150.0, paul.getWeight());
+    	Assert.assertEquals(60.0, paul.getHeight());
+    	
+    	Assert.assertEquals("Paul, a Red-colored animal. 150.0 pounds, 60.0 inches\n", paul.toString());
+    	
+    	Assert.assertEquals(true, paul.equals(paul));
+    	Assert.assertEquals(true, paul.equals(paul2));
+    	Assert.assertEquals(false, paul.equals(paul3));
+    	Assert.assertEquals(false, paul.equals(objectivePaul));
+    	Assert.assertEquals(false, boringPaul.equals(objectivePaul));
+    }
 }
 
